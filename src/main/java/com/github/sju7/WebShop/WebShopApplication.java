6895f1abe8231c7abb7c1ea8abd187c8b5b17d66
sju7 <sjur@hotmail.no>
2021-11-12 20:23:10 +0100

    first commit

---
+++ src/main/java/com/github/sju7/WebShop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package com.github.sju7.WebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
