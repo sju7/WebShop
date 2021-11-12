30cca0523fa108ac41e6ee5b9db702e7f2ce290b
sju7 <sjur@hotmail.no>
2021-11-12 22:57:33 +0100

    inmem users

--- src/test/java/com/github/sju7/WebShop/config/SecurityConfig.java
+++ src/test/java/com/github/sju7/WebShop/config/SecurityConfig.java
@@ -1,17 +1,36 @@
 package com.github.sju7.WebShop.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .hasAnyRole("USER","ADMIN")
+                .and()
+                .formLogin();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
     }
 }
